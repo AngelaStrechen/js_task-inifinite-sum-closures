0b3e39d83a8ab739375a5e321e34859b9ead22c1
Angela <angelastrechen1@mail.com>
2026-01-16 23:13:45 +0100

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  function adder(...args) {
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0; // скидаємо суму після виклику без аргументів
+
+      return result;
+    }
+
+    sum += args[0]; // додаємо перший аргумент
+
+    return adder; // повертаємо саму себе для ланцюгового виклику
+  }
+
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
